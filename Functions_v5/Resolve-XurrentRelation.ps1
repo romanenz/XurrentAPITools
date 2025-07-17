d0ac08933059c571940baf0d1d34a227597ed557
romanenz <roman@enz.family>
2025-07-17 12:10:55 +0200

    add all parameter to resolve id by source

--- Functions_v5/Resolve-XurrentRelation.ps1
+++ Functions_v5/Resolve-XurrentRelation.ps1
@@ -1,33 +1,53 @@
 ﻿function Resolve-XurrentRelation
 {
-	[CmdletBinding()]
+	[CmdletBinding(DefaultParameterSetName = 'id')]
 	param (
-		[Parameter(Mandatory = $true)]
+		[Parameter(Mandatory = $true, ParameterSetName = 'id')]
+		[Parameter(Mandatory = $true, ParameterSetName = 'all')]
 		[ValidateScript({ $_ -in $script:XurrentDataTypes })]
 		[string]$Type,
-		[Parameter(Mandatory = $true)]
+		[Parameter(Mandatory = $true, ParameterSetName = 'id')]
+		[Parameter(Mandatory = $true, ParameterSetName = 'all')]
 		[ValidateScript({ $null -ne $script:XurrentAuth.$_ })]
 		[string]$SourceEnvironment,
-		[Parameter(Mandatory = $true)]
+		[Parameter(Mandatory = $true, ParameterSetName = 'id')]
+		[Parameter(Mandatory = $true, ParameterSetName = 'all')]
 		[ValidateScript({ $null -ne $script:XurrentAuth.$_ })]
 		[string]$DestinationEnvironment,
-		[Parameter(Mandatory = $true)]
-		[int[]]$ID
+		[Parameter(Mandatory = $true, ParameterSetName = 'id')]
+		[int[]]$ID,
+		[Parameter(Mandatory = $true, ParameterSetName = 'all')]
+		[ValidateScript({ $PSVersionTable.PSVersion -ge $Script:MinimalV7Version })]
+		[switch]$All
 		
 	)
 	try
 	{
-		$SourceData = Get-XurrentData -Type $Type -Environment $SourceEnvironment -ID $ID -Parameter "fields=id, source, sourceID"
+		if ($All)
+		{
+			$SourceData = Export-XurrentData -Environment $SourceEnvironment -Type $Type | Import-Csv | Select-Object name, ID, @{ l = "source"; e = { $_."Source" } }, @{ l = "sourceID"; e = { $_."Source ID" } }
+			$DestinationData = Export-XurrentData -Environment $DestinationEnvironment -Type $Type | Import-Csv | Select-Object name, ID, @{ l = "source"; e = { $_."Source" } }, @{ l = "sourceID"; e = { $_."Source ID" } }
+		}
+		else
+		{
+			$SourceData = Get-XurrentData -Type $Type -Environment $SourceEnvironment -ID $ID -Parameter "fields=id, source, sourceID"
+			$DestinationData = @()
+			foreach ($Item in $SourceData)
+			{
+				$DestinationData += Get-XurrentData -Type $Type -Environment $DestinationEnvironment -Parameter "source=$($Item.source)&sourceID=$($Item.sourceID)&fields=id, source, sourceID"
+			}
+		}
 		[System.Collections.ArrayList]$RelationData = @()
 		
 		foreach ($Item in $SourceData)
 		{
 			$null = $RelationData.add([PSCustomObject]@{
-						IDsource	  = $Item.id
-						SoruceID	  = $Item.sourceID
-						source	      = $Item.source
-						IDdestination = (Get-XurrentData -Type $Type -Environment $DestinationEnvironment -Parameter "source=$($Item.source)&sourceID=$($Item.sourceID)&fields=id, source, sourceID").id
-					})
+					IDsource	  = $Item.id
+					IDdestination = ($DestinationData | Where-Object { $_.source -eq $Item.source -and $_.sourceID -eq $Item.sourceID }).id
+					SoruceID	  = $Item.sourceID
+					source	      = $Item.source
+					name		  = $Item.name
+				})
 		}
 		return $RelationData
 	}
